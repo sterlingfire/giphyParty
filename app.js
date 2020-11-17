aa5cd41a8b62fcc882899234fd37c88b07a2dec7
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-11-17 15:59:36 -0800

    implemented search and remove

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 console.log("Let's get this party started!");
+
+$("searchButton").on("click", search);
+$("removeButton").on("click", remove);
+
+async function search(evt) {
+  console.log("submit function!")
+  evt.preventDefault();
+  let query = $("#searchText").val();
+  let url = await searchGiphy(query);
+  addGif(url);
+}
+
+function remove() {
+  $("#gifs").empty();
+}
+
+async function searchGiphy(query) {
+  let apiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+  let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}`);
+  console.log(`${response}`);
+  console.log(response.data.url);
+  return response.data.url;
+}
+
+function addGif(url) {
+  $("#gifs")
+    .append("<div>")
+    .append("<a>")
+    .attr("href", url);
+}
