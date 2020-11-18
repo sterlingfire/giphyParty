c94b2860f91d2b01a15862e75b11e872161af9b9
Sean Kim <sean.m.kim@gmail.com>
2020-11-17 17:09:28 -0800

    Working giphy party after code review

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
+"use strict";
 console.log("Let's get this party started!");
 
-$("searchButton").on("click", search);
-$("removeButton").on("click", remove);
-
+/*Gets URL of search input and adds gif */
 async function search(evt) {
-  console.log("submit function!")
   evt.preventDefault();
   let query = $("#searchText").val();
   let url = await searchGiphy(query);
+  
   addGif(url);
 }
 
+/**Removes all gifs from DOM */
 function remove() {
   $("#gifs").empty();
 }
 
+/**Searches giphy.com and returns URL of gif to embed in DOM */
 async function searchGiphy(query) {
-  let apiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
-  let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}`);
-  console.log(`${response}`);
-  console.log(response.data.url);
-  return response.data.url;
+  const apiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"   //make it a global variable
+  let searchURL = `http://api.giphy.com/v1/gifs/search`;
+  let response = await axios.get(searchURL,{params : {api_key : apiKey, q : query}});
+
+  return response.data.data[0].images.original.url;
 }
 
+/**Displays the gif in the DOM */
 function addGif(url) {
-  $("#gifs")
-    .append("<div>")
-    .append("<a>")
-    .attr("href", url);
+  $("#gifs").append("<div>");
+  $("#gifs>div:last-child").append("<img>");
+  $("#gifs>div:last-child>img").attr("src", url);
 }
+
+/**Add event listeners to search and remove buttons */
+$("#searchGiphy").on("submit", search);
+$("#removeButton").on("click", remove);
